4593c1dd011def38d14ae2c6c38d0c8fdb823675
hhswnt02-crypto <hhswnt02@gmail.com>
2026-09-25 00:51:12 +0100

    Update main.dart

--- main.dart
+++ main.dart
@@ -385,7 +385,7 @@ class _HomeState extends State<Home> {
         Text(a.title, maxLines: 1, overflow: TextOverflow.ellipsis,
           style: const TextStyle(fontWeight: FontWeight.bold)),
         Text(a.score == null ? a.type : '⭐ ${a.score}', style: const TextStyle(color: Colors.white54)),
-      ]),
+     ),
   );
 }
 
